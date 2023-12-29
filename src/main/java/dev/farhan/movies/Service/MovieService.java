b48266c339e5899324f0f1041cc59dfea0f96327
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-29 20:03:06 +0530

    commit

--- src/main/java/dev/farhan/movies/Service/MovieService.java
+++ src/main/java/dev/farhan/movies/Service/MovieService.java
@@ -2,10 +2,12 @@ package dev.farhan.movies.Service;
 
 import dev.farhan.movies.Model.Movie;
 import dev.farhan.movies.Repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie>allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
